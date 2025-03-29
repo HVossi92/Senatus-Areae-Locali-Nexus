5fe4aa7fc8dfe9fcd2498234e09a351b8cf24328
Hendrik Voßkamp <hendrik.vosskamp@gmail.com>
2025-03-29 15:44:38 +0100

    Localize dependencies

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module senatus
 
 go 1.23.2
 
-require github.com/a-h/templ v0.3.857 // indirect
+require github.com/a-h/templ v0.3.857
--- src/main.go
+++ src/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"senatus/src/templates"
+
+	"github.com/a-h/templ"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Serve static files from the "static" directory
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	component := templates.Index()
+	http.Handle("/", templ.Handler(component))
+
+	fmt.Println("Server listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
